f01ccd7dee7190ea5996ece3cdeeaffcec13d54d
Diana Ostapenko <Diana_Ostapenko@epam.com>
2018-10-19 17:26:25 +0300

    Added new tests

--- HelloWorld/NUnitHelloWorldTests/UNitTests.cs
+++ HelloWorld/NUnitHelloWorldTests/UNitTests.cs
@@ -7,11 +7,35 @@ namespace NUnitHelloWorldTests
     [TestFixture]
     public class UNitTests
     {
-            [Test]
-            public void TestMethod1()
-            {
-                TestClass testClass = new TestClass();
-                Assert.AreEqual("Hello World", testClass.Test());
+        [Test]
+        public void TestMethod1()
+        {
+            TestClass testClass = new TestClass();
+            Assert.AreEqual("Hello World", testClass.Test());
         }
+        [Test]
+        public void TestMethod2()
+        {
+            TestClass testClass = new TestClass();
+            Assert.AreEqual("Hello World", testClass.Test());
         }
+        [Test]
+        public void TestMethod3()
+        {
+            TestClass testClass = new TestClass();
+            Assert.AreEqual("Hello World", testClass.Test());
+        }
+        [Test]
+        public void TestMethod4()
+        {
+            TestClass testClass = new TestClass();
+            Assert.AreEqual("Hello World", testClass.Test());
+        }
+        [Test]
+        public void TestMethodFailture()
+        {
+            TestClass testClass = new TestClass();
+            Assert.AreEqual("Hello World2", testClass.Test());
+        }
+    }
     }
