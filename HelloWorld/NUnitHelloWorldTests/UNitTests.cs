04b99a818ffe712dae930aaa5f0f5241a3b30425
Diana Ostapenko <Diana_Ostapenko@epam.com>
2018-10-19 13:27:19 +0300

    Added NUnit Tests

---
+++ HelloWorld/NUnitHelloWorldTests/UNitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorld;
+using NUnit.Framework;
+
+namespace NUnitHelloWorldTests
+{
+    [TestFixture]
+    public class UNitTests
+    {
+            [Test]
+            public void TestMethod1()
+            {
+                Assert.AreEqual("Hello World", Program.CreateMessage());
+            }
+        }
+    }
